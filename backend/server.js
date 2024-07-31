0fc0f7af62b7880e24d676b3e215050a8c930ca4
J4-R0 <michel-gerald.kangmeni@epitech.eu>
2024-07-31 14:09:57 +0200

    frontend

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,25 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/todos', require('./routes/todoRoutes'))
-//app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/form', require('./routes/formRoutes'));
-app.get('/login', (req, res) => {
+/*app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'login.html'));
-});
+});*/
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+        )
+    );
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+}
+
 app.use(errorHandler);
 
 app.listen(port, () => {
